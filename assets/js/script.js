69aa9021ed58f49a80c0a8d4d2212be5cf49f7be
nkartikaxioned <kartikn@axioned.com>
2023-09-29 12:29:23 +0530

    js 20%

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,29 @@
 
 const arrList=document.querySelector('.list'),
       arrposition=document.querySelectorAll('.position'),
-      buttonprev=document.querySelector('previous'),
-      buttonnext=document.querySelector('next');
+      buttonprev=document.querySelector('.prev-next-container .previous'),
+      buttonnext=document.querySelector('.prev-next-container .next');
+
+
+window.addEventListener('load', () => {
+let a=0;
+    buttonnext.addEventListener('click',()=>{
+        console.log("in button n")
+        if(a<4){
+            console.log("in first if")
+            a++;
+        }
+         if(a<1){
+            console.log("in back")
+            buttonnext.style.background="grey";
+        }
+    })
+    buttonprev.addEventListener('click',()=>{
+        if(a>=3){
+            a--;
+        }
+    })
+})
 
 
 
