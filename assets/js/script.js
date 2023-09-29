4b74845ddb4bde5089fe05e63602ed000f557dbd
nkartikaxioned <kartikn@axioned.com>
2023-09-29 11:32:12 +0530

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,10 @@
 
 */
 
-
+const arrList=document.querySelector('.list'),
+      arrposition=document.querySelectorAll('.position'),
+      buttonprev=document.querySelector('previous'),
+      buttonnext=document.querySelector('next');
 
 
 
